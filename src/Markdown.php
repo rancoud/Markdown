af18d0c5647dd98fa697a1c141347dead2193c13
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2018-12-11 00:48:40 +0100

    add tests

--- src/Markdown.php
+++ src/Markdown.php
@@ -9,10 +9,45 @@ namespace Rancoud\Markdown;
  */
 class Markdown
 {
-    /**
-     * Markdown constructor.
-     */
+    protected $renderText = '';
+
     public function __construct()
     {
+        //
+    }
+
+    public function render(string $content): string
+    {
+        $this->renderText = "";
+
+        $lines = explode("\r\n", $content);
+        $countLines = count($lines);
+        for ($i = 0; $i < $countLines; $i++) {
+            $this->treatLine($lines[$i]);
+        }
+
+        return $this->renderText;
+    }
+
+    protected function treatLine(string $line): void
+    {
+        $this->tokenizer($line);
+    }
+
+    protected function tokenizer(string $string)
+    {
+        $str = '';
+        $len = mb_strlen($string);
+
+        for ($i = 0; $i < $len; $i++) {
+            switch ($string{$i}) {
+                case '#':
+                    $str = '';
+                    break;
+                default:
+                    $str.= $string{$i};
+                    break;
+            }
+        }
     }
 }
--- tests/MarkdownTest.php
+++ tests/MarkdownTest.php
@@ -17,4 +17,35 @@ class MarkdownTest extends TestCase
         new Markdown();
         static::assertTrue(true);
     }
+
+    /**
+     * @dataProvider data
+     * @param $filename
+     */
+    public function testRender($filename)
+    {
+        $dirpath = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
+
+        $input = file_get_contents($dirpath . $filename . '.md');
+        $output = file_get_contents($dirpath . $filename . '.html');
+
+        $markdown = new Markdown();
+
+        static::assertSame($output, $markdown->render($input));
+    }
+
+    public function data()
+    {
+        $data = [];
+
+        $dirpath = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
+        $len = strlen($dirpath);
+        $paths = glob($dirpath . '*.md');
+        foreach ($paths as $path) {
+            $name = substr($path, $len, -3);
+            $data[$name] = ['name' => $name];
+        }
+
+        return $data;
+    }
 }
