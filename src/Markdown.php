3d83aed19d586e94c77b2a4aa2d4fa182949fbe2
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2019-01-08 01:50:58 +0100

    code + header

--- src/Markdown.php
+++ src/Markdown.php
@@ -13,6 +13,8 @@ class Markdown
     protected $lines = [];
     protected $countLines = 0;
     protected $currentIndex = 0;
+    protected $countRunes = 0;
+    protected $currentRune = 0;
 
     protected $depths = [];
     protected $types = [
@@ -27,6 +29,8 @@ class Markdown
             'strike',
             'link',
             'image',
+            'simple_code',
+            'double_code'
         ]
     ];
 
@@ -37,11 +41,11 @@ class Markdown
 
     public function render(string $content): string
     {
-        $this->renderText = "";
+        $this->renderText = '';
 
-        $this->lines = explode("\r\n", $content);
-        $this->countLines = count($this->lines);
-        for ($this->currentIndex = 0; $this->currentIndex < $this->countLines; $this->currentIndex++) {
+        $this->lines = \preg_split('/\R/', $content);
+        $this->countLines = \count($this->lines);
+        for ($this->currentIndex = 0; $this->currentIndex < $this->countLines; ++$this->currentIndex) {
             $this->treatLine($this->lines[$this->currentIndex]);
         }
 
@@ -54,13 +58,20 @@ class Markdown
             $line = $this->removeUselessLeadingSpaces($line);
         }
 
-        $this->tokenizer($line);
+        $header = $this->handleHeader($line);
+        if ($header !== null) {
+            $this->renderText .= $header;
+        } else {
+            $this->tokenizer($line);
+        }
+
+        $this->renderText .= "\r\n";
     }
 
     protected function removeUselessLeadingSpaces(string $line): string
     {
-        if (strspn($line, ' ') < 4) {
-            $line = ltrim($line, ' ');
+        if (\strspn($line, ' ') < 4) {
+            $line = \ltrim($line, ' ');
         }
 
         return $line;
@@ -68,28 +79,137 @@ class Markdown
 
     protected function tokenizer(string $string)
     {
-        $str = '';
-        $len = mb_strlen($string);
+        $this->countRunes = \mb_strlen($string);
 
-        for ($i = 0; $i < $len; $i++) {
-            switch ($string{$i}) {
+        for ($this->currentRune = 0; $this->currentRune < $this->countRunes; ++$this->currentRune) {
+            $nextOne = $string[$this->currentRune + 1] ?? null;
+            $nextTwo = $string[$this->currentRune + 2] ?? null;
+            switch ($string[$this->currentRune]) {
+                case '`':
+                    $this->renderText .= $this->handleCode($nextOne, $nextTwo);
+                    break;
                 case '#':
-                    $str = '';
+                    $this->renderText .= '#';
+                    break;
+                case '*':
+                    $this->renderText .= '*';
+                    break;
+                case '_':
+                    $this->renderText .= '_';
+                    break;
+                case '~':
+                    $this->renderText .= '~';
                     break;
                 default:
-                    $str.= $string{$i};
+                    $this->renderText .= $string[$this->currentRune];
                     break;
             }
-
-
         }
     }
 
-    protected function isIn(string $type, $index): bool
+    // TODO : block level and multiple quotes patterns ```` ````
+    protected function handleCode(?string $nextOne, ?string $nextTwo): string
+    {
+        $isDoubleTick = false;
+        if ($nextOne !== null && $nextOne === '`') {
+            $isDoubleTick = true;
+            ++$this->currentRune;
+
+            if ($this->isIn('inline', 'simple_code')) {
+                return '``';
+            }
+
+            // Manage:
+            // ``sth sth``
+            if (!$this->isIn('inline', 'double_code')) {
+                $this->addDepth('inline', 'double_code');
+
+                if ($nextTwo === ' ') {
+                    ++$this->currentRune;
+                }
+
+                return '<code>';
+            }
+            $this->subDepth('inline', 'double_code');
+
+            if ($this->renderText[\mb_strlen($this->renderText) - 1] === ' ') {
+                $this->renderText = \mb_substr($this->renderText, 0, \mb_strlen($this->renderText) - 1);
+            }
+
+            return '</code>';
+        }
+        // Manage:
+        // `` ` ``
+        if ($this->isIn('inline', 'double_code')) {
+            return '`';
+        }
+
+        // Manage:
+        // `sth `` sth`
+        if ($isDoubleTick && $this->isIn('inline', 'simple_code')) {
+            return '``';
+        }
+
+        /* Manage:
+         * a `code span`
+         * `this is also a codespan` trailing text
+         * `and look at this one!`
+         * */
+        if (!$this->isIn('inline', 'simple_code')) {
+            $this->addDepth('inline', 'simple_code');
+
+            return '<code>';
+        }
+        $this->subDepth('inline', 'simple_code');
+
+        return '</code>';
+    }
+
+    protected function handleHeader(string $line): ?string
+    {
+        $output = null;
+        $headerLevelFound = 0;
+
+        for ($headerLevel = 6; $headerLevel > 0; --$headerLevel) {
+            $closedHeaderMark = \str_repeat('#', $headerLevel);
+            if (\strncmp($closedHeaderMark . ' ', $line, $headerLevel + 1) === 0) {
+                $output = '<h' . $headerLevel . '>' . \trim(\mb_substr($line, $headerLevel + 1)) . '</h' . $headerLevel . '>';
+                $headerLevelFound = $headerLevel;
+                break;
+            } elseif ($closedHeaderMark === $line) {
+                return '<h' . $headerLevel . '></h' . $headerLevel . '>';
+            }
+        }
+
+        if ($headerLevelFound > 0 && $output[\mb_strlen($output) - 6] === '#') {
+            $closedHeaderMark = \str_repeat('#', $headerLevelFound);
+            if (\mb_substr($output, -($headerLevelFound + 5)) === ($closedHeaderMark . '</h' . $headerLevelFound . '>')) {
+                $output = \trim(\mb_substr($output, 0, \mb_strlen($output) - ($headerLevelFound + 5))) . '</h' . $headerLevelFound . '>';
+            }
+        }
+
+        return $output;
+    }
+
+    //region Depth Managing
+
+    /**
+     * @param string $type
+     * @param string $index
+     *
+     * @return bool
+     */
+    protected function isIn(string $type, string $index): bool
     {
         return $this->getDepth($type, $index) > 0;
     }
 
+    /**
+     * @param string $type
+     * @param string $index
+     *
+     * @return int
+     */
     protected function getDepth(string $type, string $index): int
     {
         return $this->depths[$type][$index];
@@ -101,23 +221,39 @@ class Markdown
             foreach ($indexes as $index) {
                 $this->setDepth($type, $index);
             }
-
         }
     }
 
+    /**
+     * @param string $type
+     * @param string $index
+     */
     protected function setDepth(string $type, string $index): void
     {
-        $this->depths[$type] = [];
+        if (!isset($this->depths[$type])) {
+            $this->depths[$type] = [];
+        }
+
         $this->depths[$type][$index] = 0;
     }
 
+    /**
+     * @param string $type
+     * @param string $index
+     */
     protected function addDepth(string $type, string $index): void
     {
-        $this->depths[$type][$index]++;
+        ++$this->depths[$type][$index];
     }
 
+    /**
+     * @param string $type
+     * @param string $index
+     */
     protected function subDepth(string $type, string $index): void
     {
-        $this->depths[$type][$index]--;
+        --$this->depths[$type][$index];
     }
+
+    //endregion
 }
